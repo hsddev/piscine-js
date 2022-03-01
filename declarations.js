11ea3008ec1d3bb0080be4c0192c0eb1e7424a95
hsddev <75630291+hsddev@users.noreply.github.com>
2022-03-01 12:31:47 +0000

    declarations

---
+++ declarations.js
@@ -0,0 +1,18 @@
+var escapeStr = "'" + "\\" + "`" + "/" + '"';
+console.log(escapeStr);
+var arr = [4, 2];
+var obj = {
+    str: "Hello world",
+    num: 30,
+    bool: true,
+    undef: undefined,
+};
+
+var nested = {
+    arr: [4, undefined, "2"],
+    obj: {
+        str: "Hey",
+        num: 20,
+        bool: false,
+    },
+};
