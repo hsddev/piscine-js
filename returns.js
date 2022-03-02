1bd9073a0c0f7e44eb3772d34d72f5a47de1d242
hsddev <75630291+hsddev@users.noreply.github.com>
2022-03-02 12:30:05 +0000

    add some functions

---
+++ returns.js
@@ -0,0 +1,5 @@
+// id function that takes one argument and return it
+const id = (id) => id;
+
+// getLength that takes an array or a string and return its length
+const getLength = (str) => str.length;
