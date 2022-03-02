d91c11aef52c5a05e64c3d7a7c9b568b9b207d1e
hsddev <75630291+hsddev@users.noreply.github.com>
2022-03-02 12:13:27 +0000

    do some test

--- mutability.js
+++ mutability.js
@@ -1,5 +1,11 @@
 var clone1 = Object.freeze(Object.assign({}, person));
 var clone2 = Object.freeze({ ...person });
-var samePerson = { ...person };
 
-person.age--;
+// Increase age property
+person.age++;
+
+// samePerson object property values changes with person property values
+var samePerson = person;
+
+// Change country property
+person.country = "FR";
