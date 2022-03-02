a212e39e132e7b945350a802ea853e68af23634c
hsddev <75630291+hsddev@users.noreply.github.com>
2022-03-02 12:24:57 +0000

    write some functions

---
+++ more-or-less.js
@@ -0,0 +1,10 @@
+// more that takes 1 argument and add 1 to it
+const more = (num) => num++;
+
+// less function that takes 1 argument and substract 1 to it
+const less = (num) => num--;
+
+// add function that takes 2 arguments and add them
+const add = (num1, num2) => num1 + num2;
+// sub function that takes 2 arguments and substract them
+const sub = (num1, num2) => num1 - num2;
