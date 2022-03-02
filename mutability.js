fc35b73344c59fbec9211ab619c838720e9c44e0
hsddev <75630291+hsddev@users.noreply.github.com>
2022-03-02 11:56:11 +0000

    delete changes 2

--- mutability.js
+++ mutability.js
@@ -1,6 +1,5 @@
 var clone1 = Object.freeze(Object.assign({}, person));
 var clone2 = Object.freeze({ ...person });
+var samePerson = { ...person };
 
 person.age--;
-
-var samePerson = { ...person };
