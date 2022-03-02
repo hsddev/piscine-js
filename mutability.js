dae232c1bc7db23023d125b7c776aaa4084fe167
hsddev <75630291+hsddev@users.noreply.github.com>
2022-03-02 11:45:59 +0000

    add mutability.js

---
+++ mutability.js
@@ -0,0 +1,11 @@
+var person = {};
+
+person.country = "FR";
+person.age = 17;
+
+var clone1 = Object.freeze(Object.assign({}, person));
+var clone2 = Object.freeze({ ...person });
+
+person.age--;
+
+var samePerson = { ...person };
