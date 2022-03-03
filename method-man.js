38a7fe59ad22cfe873930a776cb1883be648c2af
hsddev <75630291+hsddev@users.noreply.github.com>
2022-03-03 15:53:26 +0000

    method-man

---
+++ method-man.js
@@ -0,0 +1,25 @@
+// words function that takes a string and split it into an array of strings on spaces
+const words = (str) => str.split("");
+
+// sentence function that takes an array of strings and join them with spaces
+const sentence = (arr) => arr.join("");
+
+// yell function that take a string and return it in upper case
+const yell = (str) => str.toUpperCase();
+
+// whisper function that take a string and return it in lower case and surround it with *
+const whisper = (str) => "*" + str.toLowerCase() + "*";
+
+// Create a capitalize function that takes a string and transforms it to upper case only for the first letter, and in lowercase for the rest of the string
+const capitalize = (str) => {
+    // Split the string in array for easy access/manipulation by indexing
+    Split_String = str.split("");
+    // Target the first word
+    Split_String[0] = Split_String[0].toUpperCase();
+    // Target the last word
+    Split_String[Split_String.length - 1] =
+        Split_String[Split_String.length - 1].toUpperCase();
+    // Join the array into a single word
+    Joined_Back = Split_String.join("");
+    return Joined_Back;
+};
