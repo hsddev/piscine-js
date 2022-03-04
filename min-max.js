11907520e64615f591300813a556e0d12129ed69
hsddev <75630291+hsddev@users.noreply.github.com>
2022-03-04 09:13:18 +0000

    create min max func

---
+++ min-max.js
@@ -0,0 +1,5 @@
+// max function that takes 2 number as arguments and returns the greatest
+const max = (num1, num2) => (num1 > num2 ? num1 : num2);
+
+// min function that takes 2 number as arguments and returns the lowest
+const min = (num1, num2) => (num1 < num2 ? num1 : num2);
