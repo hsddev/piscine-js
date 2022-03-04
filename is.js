9dcab41e8d328d1f0bf5cd635930f96624549223
hsddev <75630291+hsddev@users.noreply.github.com>
2022-03-04 16:17:50 +0000

    is js

---
+++ is.js
@@ -0,0 +1,14 @@
+const is = {};
+
+is.num = 0;
+is.nan = NaN;
+is.bool = true;
+is.str = "";
+("💩");
+is.undef = undefined;
+[1, Array(1), [], 2];
+is.obj = Object.create(null);
+is.def = null;
+is.fun = console.log;
+is.falsy = void 0;
+is.truthy = { length: 10 };
