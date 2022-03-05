7a7cbd48b8dc211081144551a936ebdbc246047d
hsddev <75630291+hsddev@users.noreply.github.com>
2022-03-05 11:37:58 +0000

    check the length of object

--- physics.js
+++ physics.js
@@ -15,12 +15,14 @@ t = time
 const getAcceleration = (obj) => {
     let a;
 
-    if (obj.f !== undefined && obj.m !== undefined) {
-        a = obj.f / obj.m;
-    } else if (obj.Δv && obj.Δt) {
-        a = obj.Δv / obj.Δt;
-    } else if (obj.d || obj.t) {
-        a = (2 * obj.d) / obj.t;
+    if (Object.keys(obj).length == 5) {
+        if (obj.f !== undefined && obj.m !== undefined) {
+            a = obj.f / obj.m;
+        } else if (obj.Δv && obj.Δt) {
+            a = obj.Δv / obj.Δt;
+        } else if (obj.d || obj.t) {
+            a = (2 * obj.d) / obj.t;
+        }
     } else {
         a = "impossible";
     }
