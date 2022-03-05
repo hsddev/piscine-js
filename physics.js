4ed50a50e0077ca4d30e5e139333482f7d4b08cc
hsddev <75630291+hsddev@users.noreply.github.com>
2022-03-05 12:18:24 +0000

    last fix

--- physics.js
+++ physics.js
@@ -15,20 +15,12 @@ t = time
 const getAcceleration = (obj) => {
     let a;
 
-    if (
-        Object.keys(obj).length == 2 ||
-        Object.keys(obj).length == 4 ||
-        Object.keys(obj).length == 5
-    ) {
-        if (obj.f !== undefined && obj.m !== undefined) {
-            a = obj.f / obj.m;
-        } else if (obj.Δv && obj.Δt) {
-            a = obj.Δv / obj.Δt;
-        } else if (obj.d || obj.t) {
-            a = (2 * obj.d) / obj.t;
-        } else {
-            a = "impossible";
-        }
+    if (obj.f !== undefined && obj.m !== undefined) {
+        a = obj.f / obj.m;
+    } else if (obj.Δv !== undefined && obj.Δt !== undefined) {
+        a = obj.Δv / obj.Δt;
+    } else if (obj.d !== undefined && obj.t !== undefined) {
+        a = (2 * obj.d) / obj.t;
     } else {
         a = "impossible";
     }
