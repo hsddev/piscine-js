506b74d362fc425e0087529babf67134c52ae086
hsddev <75630291+hsddev@users.noreply.github.com>
2022-03-05 11:42:40 +0000

    add some login

--- physics.js
+++ physics.js
@@ -15,13 +15,15 @@ t = time
 const getAcceleration = (obj) => {
     let a;
 
-    if (Object.keys(obj).length == 5) {
+    if (Object.keys(obj).length == 0 || Object.keys(obj).length == 3) {
         if (obj.f !== undefined && obj.m !== undefined) {
             a = obj.f / obj.m;
         } else if (obj.Δv && obj.Δt) {
             a = obj.Δv / obj.Δt;
         } else if (obj.d || obj.t) {
             a = (2 * obj.d) / obj.t;
+        } else {
+            a = "impossible";
         }
     } else {
         a = "impossible";
@@ -30,4 +32,4 @@ const getAcceleration = (obj) => {
     return a;
 };
 
-console.log(getAcceleration({ f: 2 }));
+console.log(getAcceleration({ f: 2, t: 4 }));
